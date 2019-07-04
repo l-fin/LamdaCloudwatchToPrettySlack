cdb9ab2325e8173468775b1c0f1562f1f48caa5d
insiderhj <insiderhj@gmail.com>
2019-07-04 11:37:14 +0900

    [add] gitignore, [update] comment

--- CloudwatchToPrettySlack-179b3a84-3da5-48ba-9e69-e3ffdbe7d1ed-1 2/lambda_function.py
+++ CloudwatchToPrettySlack-179b3a84-3da5-48ba-9e69-e3ffdbe7d1ed-1 2/lambda_function.py
@@ -9,6 +9,8 @@ from datetime import timedelta
 
 '''
 token : OAuth access token for bot
+channel : channel name in slack (including '#')
+timeDifference : national standard time - GMT
 '''
 
 TOKEN = os.environ['token']
@@ -18,6 +20,8 @@ TIME_DIFFERENCE = os.environ['timeDifference']
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+
+# SNS에서 받은 ComparisonOperator 정보로부터 문장 생성
 def check_comparance(oper):
     if oper == 'GreaterThanThreshold':
         return 'greater than the threshold'
@@ -28,23 +32,32 @@ def check_comparance(oper):
     else:
         return 'less than or equal to the threshold'
 
+
 def lambda_handler(event, context):
     logger.info("Event: " + str(event))
-    message = json.loads(event['Records'][0]['Sns']['Message'])
+    message = json.loads(event['Records'][0]['Sns']['Message'])     # json 형식으로 SNS 메시지 로드
     logger.info("Message: " + str(message))
-    
+
+    # 메시지에서 알람 이름, 알람 시각, 현재 상태, 트리거의 이름 등을 받아 저장
     alarm_name = message['AlarmName']
     alarm_time = message['StateChangeTime']
     new_state = message['NewStateValue']
     trigger = message['Trigger']['MetricName']
     threshold = message['Trigger']['Threshold']
 
+    # GMT 시각에 TIME_DIFFERENCE를 더해 해당 국가 시각으로 변경
     parsed_time = (parse(alarm_time) + timedelta(hours=int(TIME_DIFFERENCE))).strftime("%Y/%m/%d %H:%M:%S")
+
+    # 메시지로부터 현재 데이터 값만 파싱해 저장
     datapoint = ((message['NewStateReason'].split('['))[1].split())[0]
     comparance = check_comparance(message['Trigger']['ComparisonOperator'])
-    reason = trigger + '(' + datapoint + ')' + (' was ' if new_state == 'ALARM' else ' was not ') + comparance + '(' + str(threshold) + ')'
+
+    # 최종 메시지 생성
+    reason = trigger + '(' + datapoint + ')' + (
+        ' was ' if new_state == 'ALARM' else ' was not ') + comparance + '(' + str(threshold) + ')'
     slack = Slacker(TOKEN)
 
+    # 메시지 디자인
     attachments_dict = dict()
     attachments_dict['color'] = 'good' if new_state == 'OK' else 'danger'
     attachments_dict['pretext'] = '*```%s```*' % alarm_name
@@ -54,5 +67,6 @@ def lambda_handler(event, context):
     attachments_dict['field'] = dict()
     attachments_dict['field']['text'] = 'hello'
     attachments = [attachments_dict]
-    
+
+    # 봇 이름, 아이콘 등을 설정하여 슬랙 채널에 메시지 전송
     slack.chat.post_message(CHANNEL, username='AWS Monitoring Bot', icon_emoji=':robot_face:', attachments=attachments)
